f9e9a74e09dec98d3d52741bd76f8a0f0d9ec804
Serey002 <serey.phem.12022006@gmail.com>
2026-06-16 13:46:05 +0700

    practic

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
-// import App from './App.vue'
-// import App from './Computed.vue'
-// import App from './TaskTracking.vue'
-import App from './PersonalFinanceTracker.vue'
+import App from './App.vue'
+
 import "@/css/style.css"
 
 createApp(App).mount('#app')
